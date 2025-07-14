08230343c072a5b8e5c643a6b59a5dde5e4f46d0
Claytoon525 <claytonlarson525@gmail.com>
2025-07-14 12:46:31 -0700

    fixed

--- Project/Assets/Scripts/Contact.cs
+++ Project/Assets/Scripts/Contact.cs
@@ -7,15 +7,13 @@ public class Contact : MonoBehaviour
     public int damadge;
     public enum ItemType
     {
+        Spike,
         Arrow,
         Lava
     }
     public ItemType item;
     [SerializeField] private ItemType _itemType;
-    if(item == "Spike"")
-    {
-        damadge = 5;
-    }
+
     public PlayerController player;
     // Start is called before the first frame update
     void Start()
@@ -28,7 +26,22 @@ public class Contact : MonoBehaviour
     {
         if (other.name == "Player")
         {
-            player.health -= other.damadge;
+            //player.health -= other.damadge;
         }
+        //needs to be in collision function to work - Sam :)
+        //also use switch case for comparing 
+
+        //if(item == "Spike")
+        //{
+        //    damadge = 5;
+        //}
+        /*
+        switch(ItemType){
+            case ItemType.Spike:
+                break;
+            default: 
+                break;
+        }
+        */
     }
 }
